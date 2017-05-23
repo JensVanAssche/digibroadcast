895ed6cd759ee0a0af68e178235fa868c71532ab
Jens <jens.va@hotmail.com>
2017-05-23 16:28:38 +0200

    e

--- hoteljuist/src/hellotvxlet/HelloTVXlet.java
+++ hoteljuist/src/hellotvxlet/HelloTVXlet.java
@@ -1,5 +1,7 @@
 package hellotvxlet;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
 import javax.tv.xlet.*;
 import org.dvb.ui.DVBColor;
 import org.havi.ui.HScene;
@@ -8,39 +10,61 @@ import org.havi.ui.HSceneTemplate;
 import org.havi.ui.HScreenDimension;
 import org.havi.ui.HScreenPoint;
 import org.havi.ui.HStaticText;
+import org.havi.ui.HTextButton;
 import org.havi.ui.HVisible;
+import org.havi.ui.event.HActionListener;
 
 
-public class HelloTVXlet implements Xlet{
+public class HelloTVXlet implements Xlet, HActionListener {
 
     private HScene scene;
     private HStaticText tekstLabel;
+    HStaticText tekst2 = new HStaticText("Juist",100, 400, 550, 100);
+    HTextButton knop1 = new HTextButton("3", 100,250,100,100);
+    HTextButton knop2 = new HTextButton("5", 250,250,100,100);
+    HTextButton knop3 = new HTextButton("0", 400,250,100,100);
+    HTextButton knop4 = new HTextButton("2", 550,250,100,100);
     
     public void initXlet(XletContext context) {
-      //Template
-      HSceneTemplate sceneTemplate = new HSceneTemplate();
+      scene = HSceneFactory.getInstance().getDefaultHScene();
       
-      //Grootte
-      sceneTemplate.setPreference(HSceneTemplate.SCENE_SCREEN_DIMENSION, new HScreenDimension(1.0f, 1.0f), HSceneTemplate.REQUIRED);
-      //Positie
-      sceneTemplate.setPreference(HSceneTemplate.SCENE_SCREEN_LOCATION, new HScreenPoint(0.0f, 0.0f), HSceneTemplate.REQUIRED);
+      HStaticText tekst1 = new HStaticText("Hotel Roomservice",100, 20, 550, 100);
+      tekst1.setBackgroundMode(HVisible.BACKGROUND_FILL);
+      tekst1.setBackground(new DVBColor(168, 234, 226, 255));
+      tekst1.setForeground(new DVBColor(0, 0, 0, 255));
+      scene.add(tekst1);      
       
-      //Maak Scene
-      scene = HSceneFactory.getInstance().getBestScene(sceneTemplate);
+      HTextButton knop1=new HTextButton("Informatie en Contact",100,150,300,50);
+      knop1.setBackgroundMode(HVisible.BACKGROUND_FILL);
+      knop1.setBackground(Color.BLUE);
+      scene.add(knop1);
       
-      //Object aanmaken
-      tekstLabel = new HStaticText("TEKST");
+      HTextButton knop2=new HTextButton("Openingsuren",100,220,300,50);
+      knop2.setBackgroundMode(HVisible.BACKGROUND_FILL);
+      knop2.setBackground(Color.BLUE);
+      scene.add(knop2);
       
-      //Eigenschappen Tekstlabel
-      tekstLabel.setLocation(100, 100);
-      tekstLabel.setSize(400, 250);
-      tekstLabel.setBackground(new DVBColor(255, 255, 255, 179));
-      tekstLabel.setBackgroundMode(HVisible.BACKGROUND_FILL);
+      HTextButton knop3=new HTextButton("Eten en drinken",100,290,300,50);
+      knop3.setBackgroundMode(HVisible.BACKGROUND_FILL);
+      knop3.setBackground(Color.BLUE);
+      scene.add(knop3);
       
+      HTextButton knop4=new HTextButton("Kamer kuisen",100,360,300,50);
+      knop4.setBackgroundMode(HVisible.BACKGROUND_FILL);
+      knop4.setBackground(Color.BLUE);
+      scene.add(knop4);
       
-      //Add tekstlabel to Scene
-      scene.add(tekstLabel);
-              
+      knop1.setFocusTraversal(knop4,knop2,null,null);
+      knop2.setFocusTraversal(knop1,knop3,null,null);
+      knop3.setFocusTraversal(knop2,knop4,null,null);
+      knop4.setFocusTraversal(knop3,knop1,null,null);
+      
+      knop1.addHActionListener(this);
+      knop2.addHActionListener(this);
+      knop3.addHActionListener(this);
+      knop4.addHActionListener(this);
+      
+      knop1.requestFocus();        
     }
 
 
@@ -56,6 +80,38 @@ public class HelloTVXlet implements Xlet{
 
     public void destroyXlet(boolean unconditional) {
      
+    }
+
+    public void actionPerformed(ActionEvent arg0) {
+        System.out.println(arg0.getActionCommand());
+        
+        
+        if(arg0.getActionCommand().equals("help"))
+        {
+          scene.remove(knop1);
+          scene.remove(knop3);
+          knop2.setFocusTraversal(null, null, knop4, knop4);
+          knop4.setFocusTraversal(null, null, knop2, knop2);
+          scene.repaint();
+          return;
+        }
+        
+        if(arg0.getActionCommand().equals("Juist"))
+        {    
+          tekst2.setBackgroundMode(HVisible.BACKGROUND_FILL);
+          tekst2.setTextContent("Juist", HVisible.NORMAL_STATE);
+          tekst2.setBackground(Color.GREEN);
+          scene.add(tekst2);
+          scene.repaint();
+        }
+        else{
+          tekst2.setBackgroundMode(HVisible.BACKGROUND_FILL);
+          tekst2.setTextContent("Fout", HVisible.NORMAL_STATE);
+          tekst2.setBackground(Color.RED);
+          scene.add(tekst2);
+          scene.repaint();
+        }
+       
     }
 
 }
