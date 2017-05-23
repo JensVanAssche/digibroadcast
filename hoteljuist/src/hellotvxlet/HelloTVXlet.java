97e75dd97dc6b3ff97f9252b714580bda10a1435
Woozsh <jordy.pereira@hotmail.com>
2017-05-23 15:25:18 +0200

    deleted too much info

--- hoteljuist/src/hellotvxlet/HelloTVXlet.java
+++ hoteljuist/src/hellotvxlet/HelloTVXlet.java
@@ -1,35 +1,14 @@
 package hellotvxlet;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
 import javax.tv.xlet.*;
-import org.bluray.ui.event.HRcEvent;
-import org.davic.resources.ResourceClient;
-import org.davic.resources.ResourceProxy;
-import org.dvb.event.EventManager;
-import org.dvb.event.UserEvent;
-import org.dvb.event.UserEventListener;
-import org.dvb.event.UserEventRepository;
 import org.dvb.ui.DVBColor;
-import org.havi.ui.HBackgroundConfigTemplate;
-import org.havi.ui.HBackgroundDevice;
-import org.havi.ui.HBackgroundImage;
-import org.havi.ui.HConfigurationException;
-import org.havi.ui.HPermissionDeniedException;
 import org.havi.ui.HScene;
 import org.havi.ui.HSceneFactory;
 import org.havi.ui.HSceneTemplate;
-import org.havi.ui.HScreen;
 import org.havi.ui.HScreenDimension;
 import org.havi.ui.HScreenPoint;
 import org.havi.ui.HStaticText;
-import org.havi.ui.HStillImageBackgroundConfiguration;
-import org.havi.ui.HTextButton;
 import org.havi.ui.HVisible;
-import org.havi.ui.event.HActionListener;
-import org.havi.ui.event.HBackgroundImageEvent;
-import org.havi.ui.event.HBackgroundImageListener;
 
 
 public class HelloTVXlet implements Xlet{
