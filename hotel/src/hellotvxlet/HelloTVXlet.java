175870597a5329b146f6f6519e3e3b099127f89e
Woozsh <jordy.pereira@hotmail.com>
2017-05-23 14:58:21 +0200

    new folder

---
+++ hotel/src/hellotvxlet/HelloTVXlet.java
@@ -0,0 +1,53 @@
+package hellotvxlet;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import javax.tv.xlet.*;
+import org.bluray.ui.event.HRcEvent;
+import org.davic.resources.ResourceClient;
+import org.davic.resources.ResourceProxy;
+import org.dvb.event.EventManager;
+import org.dvb.event.UserEvent;
+import org.dvb.event.UserEventListener;
+import org.dvb.event.UserEventRepository;
+import org.havi.ui.HBackgroundConfigTemplate;
+import org.havi.ui.HBackgroundDevice;
+import org.havi.ui.HBackgroundImage;
+import org.havi.ui.HConfigurationException;
+import org.havi.ui.HPermissionDeniedException;
+import org.havi.ui.HScene;
+import org.havi.ui.HSceneFactory;
+import org.havi.ui.HScreen;
+import org.havi.ui.HStaticText;
+import org.havi.ui.HStillImageBackgroundConfiguration;
+import org.havi.ui.HTextButton;
+import org.havi.ui.HVisible;
+import org.havi.ui.event.HActionListener;
+import org.havi.ui.event.HBackgroundImageEvent;
+import org.havi.ui.event.HBackgroundImageListener;
+
+
+public class HelloTVXlet implements Xlet{
+
+    HScene scene;
+    
+    public void initXlet(XletContext context) {
+      
+    }
+
+
+    
+    public void startXlet() {
+    
+    }
+
+    public void pauseXlet() {
+     
+    }
+
+    public void destroyXlet(boolean unconditional) {
+     
+    }
+
+}
